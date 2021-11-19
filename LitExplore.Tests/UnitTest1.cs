2a05446718445359a590e02ea6c920342b2a2ffd
DavidAjken <75014951+DavidAjken@users.noreply.github.com>
2021-11-19 14:21:21 +0100

    Updated to c# 10 and .net 6

--- LitExplore.Tests/UnitTest1.cs
+++ LitExplore.Tests/UnitTest1.cs
@@ -1,14 +1,12 @@
-using System;
 using Xunit;
 
-namespace LitExplore.Tests
-{
-    public class UnitTest1
-    {
-        [Fact]
-        public void Test1()
-        {
+namespace LitExplore.Tests;
 
-        }
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
     }
 }
