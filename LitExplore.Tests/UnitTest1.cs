7ec789617753e2b7dd39afbf3c4e63b769a9edb9
cho <woldby3@hotmail.com>
2021-11-26 11:41:09 +0100

    Added neccessary packages for in-memory testing and set up some of the code

--- LitExplore.Tests/UnitTest1.cs
+++ LitExplore.Tests/UnitTest1.cs
@@ -1,12 +1,56 @@
+using System;
+using System.Threading.Tasks;
+using LitExplore.Core;
+using LitExplore.Entity;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
 using Xunit;
 
 namespace LitExplore.Tests;
-
-public class UnitTest1
+// In memory testing of publication repository
+public class PublicationRepositoryTests : IDisposable
 {
-    [Fact]
-    public void Test1()
-    {
+  private bool disposed;
+  private readonly ILitExploreContext _context;
+  private readonly PublicationRepository _repository;
 
+  public PublicationRepositoryTests()
+  {
+    var connection = new SqliteConnection("Filename=:memory:");
+    connection.Open();
+    var builder = new DbContextOptionsBuilder<LitExploreContext>();
+    builder.UseSqlite(connection);
+    var context = new LitExploreContext(builder.Options);
+    seed(context);
+    _context = context;
+    _repository = new PublicationRepository(_context);
+  }
+  
+  // init db
+  private void seed(LitExploreContext context) {
+    context.Database.EnsureCreated();
+    // seed actions here
+           
+    context.SaveChanges();
+  }
+  
+  protected virtual void Dispose(bool disposing)
+  {
+    if (!disposed)
+    {
+      if (disposing)
+      {
+        _context.Dispose();
+      }
+
+      disposed = true;
     }
+  }
+
+  public void Dispose()
+  {
+    // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+    Dispose(disposing: true);
+    GC.SuppressFinalize(this);
+  }
 }
