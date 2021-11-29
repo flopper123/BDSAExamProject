c770822b7f2abb1df4f388f46aa7d0078ae18946
Thomas <thomas.schauser@gmail.com>
2021-11-29 14:38:04 +0100

    Finished setting up basic frontend

--- LitExplore/Pages/Error/Error.cshtml.cs
+++ LitExplore/Pages/Error/Error.cshtml.cs
@@ -2,7 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace LitExplore.Pages;
+namespace LitExplore.Pages.Error;
 
 [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 [IgnoreAntiforgeryToken]
