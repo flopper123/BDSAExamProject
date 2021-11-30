3ea54521b31d69f00457abee153d549221807eea
DavidAjken <75014951+DavidAjken@users.noreply.github.com>
2021-11-30 12:47:21 +0100

    Simple graph is done and fully tested If in need of seeing a simple visualisation run PublicationGraph.Main(new string[0]);

--- Graph.Tests/GraphTests.cs
+++ Graph.Tests/GraphTests.cs
@@ -6,16 +6,16 @@ namespace Graph.Tests
 
     public class GraphTests : IDisposable
     {
-        Graph _graph;
+        PublicationGraph _graph;
         public GraphTests()
         {
-            _graph = new Graph();
+            _graph = new PublicationGraph();
         }
         //AddVertex  
         [Fact]
         public void Inserts_1_Publication_Vertex_Without_Title_Returns_False()
         {
-            var vertex = new Vertex(0, new Publication{});
+            var vertex = new Vertex(0, new Publication { });
             var add = _graph.AddVertex(vertex);
 
             var actual = _graph.NumberOfVertices();
@@ -26,7 +26,7 @@ namespace Graph.Tests
         [Fact]
         public void Inserts_1_Publication_Vertex_Increses_Number_Of_Vertices_By_1()
         {
-            var vertex = new Vertex(0, new Publication{Title = "Test"});
+            var vertex = new Vertex(0, new Publication { Title = "Test" });
             var add = _graph.AddVertex(vertex);
 
             var actual = _graph.NumberOfVertices();
@@ -39,7 +39,7 @@ namespace Graph.Tests
         public void Inserts_1_Referance_Edge_With_addEdge_Vertex_Vertex_Returns_False_With_Empty_Data()
         {
             var edge = (new Vertex(0, new Publication()), new Vertex(1, new Publication()));
-            var add = _graph.AddEdge(edge.Item1,edge.Item2);
+            var add = _graph.AddEdge(edge.Item1, edge.Item2);
 
             var actual = _graph.NumberOfEdges();
 
@@ -50,35 +50,45 @@ namespace Graph.Tests
         [Fact]
         public void Inserts_1_Referance_Edge_With_addEdge_Vertex_Vertex_Increses_Number_Of_Edges_By_1()
         {
-            var add = _graph.AddEdge(new Vertex(0, new Publication{Title = "Test 0"}), new Vertex(1, new Publication{Title = "Test 1"}));
+            var add = _graph.AddEdge(new Vertex(0, new Publication { Title = "Test 0" }), new Vertex(1, new Publication { Title = "Test 1" }));
+            var expected = 1;
+
 
             var actual = _graph.NumberOfEdges();
 
             Assert.True(add, $"Did not add The edge correctly");
-            Assert.Equal(1, actual);
+            Assert.Equal(expected, actual);
         }
-        [Fact(Skip = "Not currently being worked on")]
-        public void Inserts_1_Referance_Edge_With_addEdge_Id_Id_Increses_Number_Of_Edges_By_1()
+        [Fact]
+        public void Inserts_1_Referance_Edge_With_addEdge_Id_Id_Out_Of_Range_Returns_False()
         {
             _graph.AddVertex(new Vertex(0, new Publication()));
             _graph.AddVertex(new Vertex(1, new Publication()));
-            _graph.AddEdge(0, 1);
+            var add = _graph.AddEdge(-1, 2);
 
             var actual = _graph.NumberOfEdges();
 
-            Assert.Equal(1, actual);
+            Assert.False(add, $"Did not add The edge correctly");
+            Assert.Equal(0, actual);
+        }
+        [Fact]
+        public void Inserts_1_Referance_Edge_With_addEdge_Id_Id_Increses_Number_Of_Edges_By_1()
+        {
+            _graph.AddVertex(new Vertex(0, new Publication { Title = "T 1" }));
+            _graph.AddVertex(new Vertex(1, new Publication { Title = "T 2" }));
+            var expected = 1;
+
+            var add = _graph.AddEdge(0, 1);
+
+            var actual = _graph.NumberOfEdges();
+            Assert.True(add, $"Did not add The edge correctly");
+            Assert.Equal(expected, actual);
         }
         //GetAdj(Vertex)
-        [Fact(Skip = "Not currently being worked on")]
+        [Fact]
         public void GetAdj_Given_Vertex_Returns_Connected_Vertices()
         {
-            _graph.AddVertex(new Vertex(0, new Publication { Title = "Publication 0" }));
-            _graph.AddVertex(new Vertex(1, new Publication { Title = "Publication 1" }));
-            _graph.AddVertex(new Vertex(2, new Publication { Title = "Publication 2" }));
-
-            _graph.AddEdge(0, 1);
-            _graph.AddEdge(0, 2);
-
+            graphSeedSmall();
 
             var adj = _graph.GetAdj(new Vertex(0, new Publication { Title = "Publication 0" })).ToArray();
 
@@ -87,16 +97,10 @@ namespace Graph.Tests
             Assert.True("Publication 2" == adj[1].Data.Title, $"The first adjesent vertex was not \"Publication 2\" but {adj[0].Data.Title}");
         }
         //GetAdj(Id)
-        [Fact(Skip = "Not currently being worked on")]
+        [Fact]
         public void GetAdj_Given_Id_Returns_Connected_Vertices()
         {
-            _graph.AddVertex(new Vertex(0, new Publication { Title = "Publication 0" }));
-            _graph.AddVertex(new Vertex(1, new Publication { Title = "Publication 1" }));
-            _graph.AddVertex(new Vertex(2, new Publication { Title = "Publication 2" }));
-
-            _graph.AddEdge(0, 1);
-            _graph.AddEdge(0, 2);
-
+            graphSeedSmall();
 
             var adj = _graph.GetAdj(0).ToArray();
 
@@ -105,10 +109,87 @@ namespace Graph.Tests
             Assert.True("Publication 2" == adj[1].Data.Title, $"The first adjesent vertex was not \"Publication 2\" but {adj[0].Data.Title}");
         }
 
+        [Fact]
+        public void Known_Vertex_0_Is_Connected_To_Vertex_11()
+        {
+            graphSeedBig();
+
+            var actual = connected(0, 11);
+
+            Assert.True(actual);
+        }
+        [Fact]
+        public void Vertex_Without_Connections_Is_Not_Connected_To_The_Graph()
+        {
+            graphSeedBig();
+            _graph.AddVertex(new Vertex(12,new Publication{Title = "Publication 12"}));
+
+            var acual = connected(0,12);
+
+            Assert.False(acual,"Unconnected vertex seems to be connected");
+        }
+        //this is a simple algorithem and shuld not be used if degrees of seperation is wanted
+        private bool connected(int fromId, int toId) 
+        {
+            var marked = new bool[_graph.NumberOfVertices()];
+            var adjs = new Stack<Vertex>();
+            adjs.Push(new Vertex(0, new Publication()));
+            while (adjs.Count > 0)
+            {
+                var v = adjs.Pop();
+                if (marked[v.Id]) continue;
+                marked[v.Id] = true;
+                foreach (var vertex in _graph.GetAdj(v.Id))
+                {
+                    adjs.Push((Vertex)vertex);
+                }
+            }
+            return marked[toId];
+        }
+
+        private void graphSeedSmall()
+        {
+            _graph.AddVertex(new Vertex(0, new Publication { Title = "Publication 0" }));
+            _graph.AddVertex(new Vertex(1, new Publication { Title = "Publication 1" }));
+            _graph.AddVertex(new Vertex(2, new Publication { Title = "Publication 2" }));
+
+            _graph.AddEdge(0, 1);
+            _graph.AddEdge(0, 2);
+        }
+        private void graphSeedBig()
+        {
+            _graph.AddVertex(new Vertex(0, new Publication { Title = "Publication 0" }));
+            _graph.AddVertex(new Vertex(1, new Publication { Title = "Publication 1" }));
+            _graph.AddVertex(new Vertex(2, new Publication { Title = "Publication 2" }));
+            _graph.AddVertex(new Vertex(3, new Publication { Title = "Publication 3" }));
+            _graph.AddVertex(new Vertex(4, new Publication { Title = "Publication 4" }));
+            _graph.AddVertex(new Vertex(5, new Publication { Title = "Publication 5" }));
+            _graph.AddVertex(new Vertex(6, new Publication { Title = "Publication 6" }));
+            _graph.AddVertex(new Vertex(7, new Publication { Title = "Publication 7" }));
+            _graph.AddVertex(new Vertex(8, new Publication { Title = "Publication 8" }));
+            _graph.AddVertex(new Vertex(9, new Publication { Title = "Publication 9" }));
+            _graph.AddVertex(new Vertex(10, new Publication { Title = "Publication 10" }));
+            _graph.AddVertex(new Vertex(11, new Publication { Title = "Publication 11" }));
+            _graph.AddEdge(0, 1);
+            _graph.AddEdge(0, 2);
+            _graph.AddEdge(1, 3);
+            _graph.AddEdge(3, 5);
+            _graph.AddEdge(2, 3);
+            _graph.AddEdge(3, 4);
+            _graph.AddEdge(4, 5);
+            _graph.AddEdge(4, 6);
+            _graph.AddEdge(5, 6);
+            _graph.AddEdge(6, 7);
+            _graph.AddEdge(7, 8);
+            _graph.AddEdge(8, 0);
+            _graph.AddEdge(8, 9);
+            _graph.AddEdge(8, 10);
+            _graph.AddEdge(10, 11);
+        }
 
         public void Dispose()
         {
-            _graph = null;
+
         }
     }
 
--- LitExplore/Program.cs
+++ LitExplore/Program.cs
@@ -1,10 +1,16 @@
 ﻿using System;
+using Graph;
+using LitExplore.Entity;
+using Interfaces;
 
 namespace LitExplore
 {
-class Program {
-    public static void Main(string[] args) {
-        Console.WriteLine("Hello, World!");
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            PublicationGraph.Main(new string[0]);
+        }
     }
-}
 }
