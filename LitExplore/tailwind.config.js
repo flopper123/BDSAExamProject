0f47a0b856eef88066e87bb4a2cbb58a510efd38
Thomas <thomas.schauser@gmail.com>
2021-12-06 14:52:44 +0100

    added top bar

--- LitExplore/tailwind.config.js
+++ LitExplore/tailwind.config.js
@@ -8,7 +8,11 @@ module.exports = {
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        white: '2px 2px 0px 0px rgba(255, 255, 255, 0.8)',
+      },
+    },
   },
   variants: {
     extend: {},
