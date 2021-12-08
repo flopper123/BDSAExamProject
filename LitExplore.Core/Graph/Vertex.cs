c18e409e2da36658c671aa72fab89aa31843f680
Woldbye <33961389+Woldbye@users.noreply.github.com>
2021-12-08 16:57:53 +0100

    Merge of new graph structure into master (#18)
    * Changed Graph to work more like a tree
    
    Co-authored-by: Madx <Madx-com@users.noreply.github.com>
    Co-authored-by: Woldbye <Woldbye@users.noreply.github.com>
    
    * Changed Graph to work more like a tree
    
    Co-authored-by: Madx <Madx-com@users.noreply.github.com>
    Co-authored-by: Woldbye <Woldbye@users.noreply.github.com>
    
    * Changed structure so vertex is responsible for iterating the graph, and added tests.
    Also:
        Changed Vertex Parent and Children retrieval and setting.
        Fixed some spacing arbitrary places in code
        Fixed some unworking tests
    
    Co-authored-by: Mads (malj@itu.dk)
    Co-authroed-by: David (davj@itu.dk)
    
    * Commit includes:
       Changes vertex depth and added some tests
    
    Co-authored-by: Mads (malj@itu.dk)
    Co-authroed-by: David (davh@itu.dk)
    
    * Get enumerator on graph magic
    
    Co-authored-by: Mads <malj@itu.dk>
    
    * Graph shuld be fully functuanl
    
    Co-authored-by: Mads <malj@itu.dk
    
    * Small semantic and logical for graph, and added a comment. updates
    Also added an empty test which should be implemented to ensure the order of the Enumerable returned by Graph matches the order of a dfs traversal of the graph.
    
    Co-authored-by: DavidAjken <davh@itu.dk>
    Co-authored-by: Madx <Madx-com@users.noreply.github.com>
    Co-authored-by: Woldbye <Woldbye@users.noreply.github.com>
    Co-authored-by: Mads <malj@itu.dk>

--- LitExplore.Core/Graph/Vertex.cs
+++ LitExplore.Core/Graph/Vertex.cs
@@ -1,15 +1,95 @@
-
 namespace LitExplore.Core.Graph;
 
-public class Vertex : IVertex<PublicationDto, string>
+public class Vertex<T> : IVertex<T>
+    where T : IEquatable<T>
 {
+    private IVertex<T> _parent = null!;
 
-    public PublicationDto Data { get; init; }
-    public string Id { get; init; }
+    public T Data { get; init; }
 
-    public Vertex(string Id, PublicationDto Data)
+    // TO:DO add tests for depth
+    public UInt64 Depth { get; set; }
+    public IList<IVertex<T>> Children { get; set; } = null!;
+    public IVertex<T> Parent {
+        get { 
+            return _parent; 
+        }
+        set {
+            _parent = value;
+            if (!IsRoot()) {
+                Depth = value.Depth + 1UL;
+            }
+        }
+    }
+
+    // worst case O(N), where N is size of subtree rooted at this vertex.
+    public UInt64 Size
     {
-        this.Id = Id;
-        this.Data = Data;
+        get
+        {
+            UInt64 size = 1UL;
+            foreach (var child in Children)
+            {
+                size += child.Size;
+            }
+            return size;
+        }
+    }
+
+    public Vertex(T data)
+    {
+        Data = data;
+        Depth = 1UL;
+        Parent = this;
+        Children = new List<IVertex<T>>();
+    }
+
+    /// <param name="data"> Source data to hold in vertex </param>
+    /// <param name="parent"> A pointer to the parent for the constructed vertex </param>
+    /// <exception cref="ArgumentException"> 
+    /// Throws argument exception if input param @parent.Data equals other param @data.
+    /// </exception>
+    public Vertex(T data, IVertex<T> parent)
+    {
+        if (data.Equals(parent.Data)) {
+            throw new ArgumentException("Vertex constructor received invalid argument.\n\t" +
+                                        "Parent must not be itself; please use Vertex(T data) if you want to set a root vertex.\n");
+        }
+
+        Data = data;
+        Parent = parent;
+        Children = new List<IVertex<T>>();
+    }
+
+    public bool IsRoot() { return (Parent == this); }
+    public bool IsLeaf() { return (Size == 1UL); }
+
+    /// <summary>
+    /// Depth-First-Search for needle. 
+    /// </summary>
+    /// <param name="needle"> Target to search for  </param>
+    /// <returns></returns>
+    public IVertex<T>? Find(T needle)
+    {
+        if (Data.Equals(needle)) return this;
+
+        IVertex<T>? tar = null;
+
+        foreach (var child in this.Children)
+        {
+            tar = child.Find(needle);
+
+            if (tar != null)
+            {
+                break;
+            }
+        }
+
+        return tar;
+    }
+
+    public override string ToString()
+    {
+        return $"Vertex depth@{Depth} data@{Data.ToString()}";
     }
 }
