530edeeaa331fcf5d1b5755fd30985839989fe07
Flopper123 <thomas.schauser@gmail.com>
2021-12-09 16:03:10 +0100

    Renamed folder

--- LitExplore.Client/Program.cs
+++ LitExplore.Client/Program.cs
@@ -8,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddSingleton<GraphController>();
 
 var app = builder.Build();
 
--- LitExplore.Controllers/GraphController.cs
+++ LitExplore.Controllers/GraphController.cs
@@ -1,14 +1,17 @@
 namespace LitExplore.Controllers;
 
+using LitExplore.Controllers.Graph;
+
+using LitExplore.Entity;
+
 public class GraphController
 {
     public GraphController() {}
-
-
-
-    /*public Task<PublicationNode> GetGraphRepresentationAsync() {
+    
+    /*public Task<List<VisualGraphNode>> GetGraphRepresentationAsync() {
         // Fetch graph
-        
+        PublicationRepository.;
+
         // Find graph relations
 
         // Heuristic map
