7fc2e2022e62b6ee187ac598f4c86e3894475069
Charlotte Schack Berg <csbe@itu.dk>
2021-12-12 20:00:00 +0100

    added configs + files from the AAD - not working

--- LitExplore.Server/Program.cs
+++ LitExplore.Server/Program.cs
@@ -4,50 +4,67 @@ namespace LitExplore.Server;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using LitExplore.Server.Controllers;
-using LitExplore.UI;
+//using LitExplore.UI;
 using Microsoft.Extensions.FileProviders;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.Identity.Web;
 
 using System.Reflection;
 
-public class Program {
+public class Program
+{
 
-  public static void Main(string[] args) {
-
-    /**
-    * File that initializes the server
-    *
-    */
-
-
-    var builder = WebApplication.CreateBuilder(args);
-
-    // Add services to the container.
-    builder.Services.AddRazorPages();
-    builder.Services.AddServerSideBlazor();
-    builder.Services.AddSingleton<GraphController>();
-
-    var app = builder.Build();
-
-    // Configure the HTTP request pipeline.
-    if (!app.Environment.IsDevelopment())
+    public static void Main(string[] args)
     {
-      app.UseExceptionHandler("/Error");
-      // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-      app.UseHsts();
+
+        /**
+        * File that initializes the server
+        *
+        */
+
+
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
+        builder.Services.AddControllersWithViews(); // from AAD
+        builder.Services.AddRazorPages();
+        builder.Services.AddServerSideBlazor();
+        builder.Services.AddSingleton<GraphController>();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseWebAssemblyDebugging();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+
+
+        //app.UseBlazorFrameworkFiles(); // from AAD
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.MapBlazorHub();
+        app.MapRazorPages();
+        app.MapFallbackToPage("/_Host");
+
+        app.Run();
+
     }
-
-    app.UseHttpsRedirection();
-
-    
-
-    app.UseStaticFiles();
-
-    app.UseRouting();
-
-    app.MapBlazorHub();
-    app.MapFallbackToPage("/_Host");
-
-    app.Run();
-
-  }
 }
--- LitExplore.UI/Program.cs
+++ LitExplore.UI/Program.cs
@@ -1,5 +1,24 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using LitExplore.UI;
+// from AAD
+// not sure if main needs to go back, however
+// await builder.Build().RunAsync(); will give error
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
 
-class Program
+builder.Services.AddHttpClient("LitExplore.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+    .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+
+// Supply HttpClient instances that include access tokens when making requests to the server project
+builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("LitExplore.ServerAPI"));
+
+builder.Services.AddMsalAuthentication(options =>
 {
-  static void Main(string[] args) {}
-}
+    builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
+    options.ProviderOptions.DefaultAccessTokenScopes.Add("api://9c2ba687-9dde-4107-a865-97d4690780a1/API.Access");
+});
+
+await builder.Build().RunAsync();
