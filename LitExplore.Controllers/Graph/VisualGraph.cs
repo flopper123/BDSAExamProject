9ccbf583b85d1fb9d494dd2c3b0f1c7731ccff36
Flopper123 <thomas.schauser@gmail.com>
2021-12-21 17:45:26 +0100

    Fixed small mistake

--- LitExplore.Controllers/Graph/VisualGraph.cs
+++ LitExplore.Controllers/Graph/VisualGraph.cs
@@ -5,26 +5,10 @@ using LitExplore.Core;
 
 public class VisualGraph : PublicationGraph
 {
-    private List<VisualGraphRelationNode> _nodesAsVis = new List<VisualGraphRelationNode>();
 
     // Add graph relations on init
     public void OnInit() {
-        var updated = new Dictionary<string, PublicationNode>();
-
-        foreach (var node in Nodes) {
-            var n = node.Value.ToVisual();
-            updated.Add(node.Key, n);
-            _nodesAsVis.Add(n);
-        }
-
-        Nodes = updated;
-
-
-
-        var nodes = new List<VisualGraphRelationNode>();
-        foreach (var node in _nodesAsVis) {
-            nodes.Add(node.ToVisual());
-        } 
+        var nodes = GetNodes().Select(n => n.ToVisual()).ToList();
         foreach (var n in nodes) n.AddRelations(nodes);
         Normalize();
     }
